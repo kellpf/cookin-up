a68c769a43691ae608ff8cc19c759a8d5550d4ae
kelly <kelly.figueira@dbserver.com.br>
2024-03-18 11:50:19 -0300

    first commit

---
+++ src/components/ConteudoPrincipal.vue
@@ -0,0 +1,78 @@
+<script lang="ts">
+import SuaLista from './SuaLista.vue';
+import SelecionarIngredientes from './SelecionarIngredientes.vue';
+
+export default {
+    data() {
+        return {
+            ingredientes: ['Alho', 'Cebola', 'Tomate']
+        }
+    },
+    components: { SuaLista, SelecionarIngredientes }
+}
+</script>
+
+<template>
+    <main class="conteudo-principal">
+        <SuaLista :ingredientes="ingredientes" />
+    
+        <ul class="categorias">
+            <li>
+                <SelecionarIngredientes />
+            </li>
+        </ul>
+    </main>
+</template>
+
+<style scoped>
+.conteudo-principal {
+  padding: 6.5rem 7.5rem;
+  border-radius: 3.75rem 3.75rem 0rem 0rem;
+  background: var(--creme, #FFFAF3);
+  color: var(--cinza, #444);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5rem;
+}
+
+.sua-lista-texto {
+  color: var(--coral, #F0633C);
+  display: block;
+  text-align: center;
+  margin-bottom: 1.5rem;
+}
+
+.ingredientes-sua-lista {
+  display: flex;
+  justify-content: center;
+  gap: 1rem 1.5rem;
+  flex-wrap: wrap;
+}
+
+.lista-vazia {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 0.25rem;
+
+  color: var(--coral, #F0633C);
+  text-align: center;
+}
+
+@media only screen and (max-width: 1300px) {
+  .conteudo-principal {
+    padding: 5rem 3.75rem;
+    gap: 3.5rem;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .conteudo-principal {
+    padding: 4rem 1.5rem;
+    gap: 4rem;
+  }
+}
+</style>
---
+++ src/components/SuaLista.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import type { PropType } from 'vue';
+import Tag from './Tag.vue';
+
+export default {
+    components: { Tag },
+    props: {
+        ingredientes: { type: Array as PropType<string[]>, required: true }
+    },
+}
+</script>
+
+<template>
+    <section>
+        <span class="subtitulo-lg sua-lista-texto">
+            Sua lista:
+        </span>
+
+        <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
+            <li v-for="ingrediente in ingredientes" :key="ingrediente">
+                <Tag :key="ingrediente" :texto="ingrediente" ativa />
+            </li>
+        </ul>
+
+        <p v-else class="paragrafo lista-vazia">
+            <img src="../assets/imagens/icones/lista-vazia.svg" alt="Ícone de pesquisa">
+            Sua lista está vazia, selecione ingredientes para iniciar.
+        </p>
+    </section>
+</template>
